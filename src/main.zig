2398d553216628870c6a77a6dfa8442bc0fcc096
xiri25 <pokesaidesdios@hotmail.com>
2024-09-04 21:40:20 +0200

    Pintar en la consola es un coñazo

--- src/main.zig
+++ src/main.zig
@@ -10,4 +10,36 @@ pub fn main() void {
         bgcolor = renderer.BgColor.BLUE;
     }
     renderer.nice_print(bgcolor, renderer.TextColor.BRIGHT_WHITE, "Hola desde zig con color y colores as enum(u8)\n");
+    renderer.nice_print(renderer.BgColor.RED, renderer.TextColor.BLACK, "     \n");
+    renderer.nice_print(renderer.BgColor.BRIGHT_YELLOW, renderer.TextColor.BLACK, "     \n");
+    renderer.nice_print(renderer.BgColor.MAGENTA, renderer.TextColor.BLACK, "     \n");
+    std.debug.print("\n\n\n", .{});
+
+    {
+        for (0..4) |_| {
+            for (0..4) |_| {
+                for (0..8) |i| {
+                    var bgcolor_chess = renderer.BgColor.GREEN;
+                    if (i % 2 == 0) {
+                        bgcolor_chess = renderer.BgColor.WHITE;
+                    }
+
+                    renderer.nice_print(bgcolor_chess, renderer.TextColor.BLACK, "        ");
+                }
+                std.debug.print("\n", .{});
+            }
+
+            for (0..4) |_| {
+                for (0..8) |i| {
+                    var bgcolor_chess = renderer.BgColor.GREEN;
+                    if (i % 2 == 1) {
+                        bgcolor_chess = renderer.BgColor.WHITE;
+                    }
+
+                    renderer.nice_print(bgcolor_chess, renderer.TextColor.BLACK, "        ");
+                }
+                std.debug.print("\n", .{});
+            }
+        }
+    }
 }
